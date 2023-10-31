ac30f985fd19e693649a1590c2254f86f4a150fe
LocNguyen <nguye2lc@mail.uc.edu>
2023-10-31 13:16:07 -0400

    Add project files.

---
+++ WebApplication1/Controllers/Name.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+	public class Name : Controller
+	{
+		public JsonResult name()
+		{
+			string output = false ? "Loc Nguyen" : "Loc Nguyen";
+			return Json(output);
+		}
+
+	}
+}
